6284c19586892f552ea01faa7ee4f715a81ff0c4
JanOveBicepS <olemr16@gmail.com>
2020-09-02 21:59:08 +0200

    Task 1

--- Ole_Midtgård_Weekly1/Ole_Midtgård_Weekly1.cpp
+++ Ole_Midtgård_Weekly1/Ole_Midtgård_Weekly1.cpp
@@ -1,21 +1,47 @@
 //Ask for full name, initial, age, phone number and date of birth;
 #include <iostream>
+#include <string>
+#include <limits>
 
-int name;
-int initial;
-int age;
-int phone;
-int birth;
+
 
 int main() {
+
 	std::cout << "What is your full name?";
-	std::cin >> name;
-	std::cout << "What is the initial to your first name?";
+	std::string name;
+	std::getline(std::cin, name);
+
+
+	std::cout << name << ", What is the initial to your first name?\n";
+	char initial{};
     std::cin >> initial;
-	std::cout << "What is your age?"
-		std::cin >> age;
-	std::cout << "What is your phone number?"
-		std::cin >> phone;
-	std::cout << "What is your date of birth?"
-		std::cin >> birth;
+
+
+	std::cout << name << ", What is your age?\n";
+	int age{};
+	std::cin >> age;
+
+
+	std::cout << name << ", What is your phone number?\n";
+	int phone{};
+	std::cin >> phone;
+
+
+	std::cout << name << ", What is your date of birth?\n";
+	std::string birth{};
+	std::cin >> birth;
+
+	std::cout << "Your name " << name << "\n";
+
+	std::cout << "Your initial " << initial << "\n";
+
+	std::cout << "Your age " << age << "\n";
+
+	std::cout << "Your phone number " << phone << "\n";
+
+	std::cout << "Your birthday " << birth << "\n";
+
+
+
+	return 0;
 }
